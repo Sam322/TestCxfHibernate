ea442b5e76dba048900d26c94dbeed19bf896d3b
TZX <403056946@qq.com>
2020-05-13 10:56:21 +0800

    tzx 5/13 用户管理中加入md5加密

--- src/ts/serviceImpl/MiscService.java
+++ src/ts/serviceImpl/MiscService.java
@@ -271,7 +271,7 @@ public class MiscService implements IMiscService {
 	@Override
 	public Response saveUserInfo(UserInfo obj) {
 		System.out.println("调用了saveUserInfo方法");
-
+		obj.setPWD(MD5.MD5Encode(obj.getPWD()));
 		try {
 			userInfoDao.save(obj);
 			return Response.ok(obj).header("EntityClass", "R_UserInfo").build();
