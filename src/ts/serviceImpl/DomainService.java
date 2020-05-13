9856545f81663f130c49355e215fb9c2fa6414cc
lyy-123456 <56285831+lyy-123456@users.noreply.github.com>
2020-05-13 16:20:46 +0800

    lyy 李沈阳提交的修改唐中翔的代码

--- src/ts/serviceImpl/DomainService.java
+++ src/ts/serviceImpl/DomainService.java
@@ -825,21 +825,28 @@ public class DomainService implements IDomainService {
 
 	}
 
-	// tzx
-	@Override
-	public List<ExpressSheet> getExpressListbytransnode(String id, int status) {
-		List<ExpressSheet> expressSheets = expressSheetDao.findBy("status", status, "ID", true);
-		System.out.println(id);
-		TransNode transNode = transNodeDao.get(id);
-		String regionCode = transNode.getRegionCode();
-		for (int i = 0; i < expressSheets.size(); i++) {
-			if (!regionCode.equals(expressSheets.get(i).getSender().getRegionCode())) {
-				expressSheets.remove(i);
-				i--;
+	//tzx
+			@Override
+			public List<ExpressSheet> getExpressListbytransnode(String id,int status) {
+				List<ExpressSheet> expressSheets = expressSheetDao.findBy("status", status, "ID", true);
+				System.out.println(id);
+				TransNode transNode = transNodeDao.get(id);
+				String regionCode = transNode.getRegionCode();
+				for (int i = 0; i < expressSheets.size(); i++) {
+					if (status == 0) {
+						if (!regionCode.equals(expressSheets.get(i).getSender().getRegionCode())) {
+							expressSheets.remove(i);
+							i--;
+						}
+					}else if (status == 5) {
+						if (!regionCode.equals(expressSheets.get(i).getRecever().getRegionCode())) {
+							expressSheets.remove(i);
+							i--;
+						}
+					}
+				}
+				return expressSheets;
 			}
-		}
-		return expressSheets;
-	}
 
 	// ldq²éÑ¯°ü¹üµÄroute
 	@Override
